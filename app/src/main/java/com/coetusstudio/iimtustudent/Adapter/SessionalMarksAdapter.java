3adf9e6b290b6364971eb657f3a6885ffb104491
Nawaz <nzan576@gmail.com>
2022-07-20 20:56:49 +0530

    SessionalMarks

---
+++ app/src/main/java/com/coetusstudio/iimtustudent/Adapter/SessionalMarksAdapter.java
@@ -0,0 +1,80 @@
+package com.coetusstudio.iimtustudent.Adapter;
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.coetusstudio.iimtustudent.Model.NoticeData;
+import com.coetusstudio.iimtustudent.Model.SessionalMarks;
+import com.coetusstudio.iimtustudent.R;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+
+public class SessionalMarksAdapter extends FirebaseRecyclerAdapter<SessionalMarks,SessionalMarksAdapter.myviewholder> {
+
+
+    public SessionalMarksAdapter(@NonNull FirebaseRecyclerOptions<SessionalMarks> options)
+    {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull final SessionalMarksAdapter.myviewholder holder, @SuppressLint("RecyclerView") final int position, @NonNull final SessionalMarks SessionalMarks)
+    {
+
+
+        holder.marksTitle.setText(SessionalMarks.getSessionalTitle());
+        holder.sub1.setText(SessionalMarks.getSessionalSub1());
+        holder.sub2.setText(SessionalMarks.getSessionalSub2());
+        holder.sub3.setText(SessionalMarks.getSessionalSub3());
+        holder.sub4.setText(SessionalMarks.getSessionalSub4());
+        holder.sub5.setText(SessionalMarks.getSessionalSub5());
+        holder.sub1Marks.setText(SessionalMarks.getSessionalMarks1());
+        holder.sub2Marks.setText(SessionalMarks.getSessionalMarks2());
+        holder.sub3Marks.setText(SessionalMarks.getSessionalMarks3());
+        holder.sub4Marks.setText(SessionalMarks.getSessionalMarks4());
+        holder.sub5Marks.setText(SessionalMarks.getSessionalMarks5());
+
+
+
+    } // End of OnBindViewMethod
+
+    @NonNull
+    @Override
+    public SessionalMarksAdapter.myviewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.singlerowsessional,parent,false);
+        return new SessionalMarksAdapter.myviewholder(view);
+    }
+
+
+    class myviewholder extends RecyclerView.ViewHolder
+    {
+        TextView marksTitle, sub1, sub2, sub3, sub4, sub5, sub1Marks, sub2Marks, sub3Marks, sub4Marks, sub5Marks;
+        public myviewholder(@NonNull View itemView)
+        {
+            super(itemView);
+            marksTitle=itemView.findViewById(R.id.marksTitle);
+            sub1=itemView.findViewById(R.id.sub1);
+            sub2=itemView.findViewById(R.id.sub2);
+            sub3=itemView.findViewById(R.id.sub3);
+            sub4=itemView.findViewById(R.id.sub4);
+            sub5=itemView.findViewById(R.id.sub5);
+            sub1Marks=itemView.findViewById(R.id.sub1Marks);
+            sub2Marks=itemView.findViewById(R.id.sub2Marks);
+            sub3Marks=itemView.findViewById(R.id.sub3Marks);
+            sub4Marks=itemView.findViewById(R.id.sub4Marks);
+            sub5Marks=itemView.findViewById(R.id.sub5Marks);
+
+        }
+    }
+
+
+}
--- app/src/main/java/com/coetusstudio/iimtustudent/Sessional_Assignment_Marks.java
+++ app/src/main/java/com/coetusstudio/iimtustudent/Sessional_Assignment_Marks.java
@@ -1,14 +1,53 @@
 package com.coetusstudio.iimtustudent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.coetusstudio.iimtustudent.Adapter.LectureAdapter;
+import com.coetusstudio.iimtustudent.Adapter.SessionalMarksAdapter;
+import com.coetusstudio.iimtustudent.Model.Lecture;
+import com.coetusstudio.iimtustudent.Model.SessionalMarks;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Sessional_Assignment_Marks extends AppCompatActivity {
+
+    RecyclerView recviewSessional;
+    SessionalMarksAdapter sessionalMarksAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sessional_assignment_marks);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(Sessional_Assignment_Marks.this);
+        recviewSessional=(RecyclerView)findViewById(R.id.rcSessionalMarks);
+        recviewSessional.setLayoutManager(linearLayoutManager);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+
+        FirebaseRecyclerOptions<SessionalMarks> options =
+                new FirebaseRecyclerOptions.Builder<SessionalMarks>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("SessionalMarks"), SessionalMarks.class)
+                        .build();
+
+        sessionalMarksAdapter=new SessionalMarksAdapter(options);
+        recviewSessional.setAdapter(sessionalMarksAdapter);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        sessionalMarksAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        sessionalMarksAdapter.stopListening();
+    }
+
 }
